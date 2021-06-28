800d0684526f39cc1d43947bc8478ba4e5b4eeaf
Andrew <andrew30511@gmail.com>
2021-06-28 22:06:50 +0400

    28-06-21

---
+++ LeetCode/Interesting.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    interface IFirst
+    {
+        void Test();
+    }
+
+    interface ISecond
+    {
+        void Test();
+    }
+
+    public class Dog : IFirst, ISecond
+    {
+        void IFirst.Test()
+        {
+            Console.WriteLine("I am first dog");
+        }
+
+        void ISecond.Test()
+        {
+            Console.WriteLine("I am second dog");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Dog...Just dog");
+        }
+    }
+
+    public class Interesting
+    {
+        public Interesting()
+        {
+            var dog = new Dog();
+            dog.Test();
+            IFirst iFirst = (IFirst) dog;
+            iFirst.Test();
+            ISecond iSecond = (ISecond)dog;
+            iSecond.Test();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -104,6 +104,8 @@ namespace LeetCode
 
         static void Main(string[] args)
         {
+            var tst = new Interesting();
+            return;
             HalvesAreAlike("textbook");
         }
 
